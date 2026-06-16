1de547ffa5715b4a37e37341c6aafa9a931100ec
unknown <ananttyagi799@gmail.com>
2026-06-16 12:44:17 +0530

    Improved Low Latency and some path optimizations

--- FrontEnd/src/utils/apiPaths.js
+++ FrontEnd/src/utils/apiPaths.js
@@ -2,29 +2,29 @@ export const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 export const API_PATHS = {
   AUTH: {
-    LOGIN: "api/v1/auth/login",
-    REGISTER: "api/v1/auth/register",
-    GET_USER_INFO: "api/v1/auth/getUser",
-    DELETE_ACCOUNT: "api/v1/auth/deleteAccount",
+    LOGIN: "/api/v1/auth/login",
+    REGISTER: "/api/v1/auth/register",
+    GET_USER_INFO: "/api/v1/auth/getUser",
+    DELETE_ACCOUNT: "/api/v1/auth/deleteAccount",
   },
   DASHBOARD: {
-    GET_DATA: "api/v1/dashboard",
+    GET_DATA: "/api/v1/dashboard",
   },
   INCOME: {
-    ADD_INCOME: "api/v1/income/add",
-    GET_ALL_INCOME: "api/v1/income/get",
-    DELETE_INCOME: (incomeId) => `api/v1/income/${incomeId}`,
-    DELETE_ALL_INCOME: "api/v1/income/all",
-    DOWNLOAD_INCOME: "api/v1/income/downloadexcel",
+    ADD_INCOME: "/api/v1/income/add",
+    GET_ALL_INCOME: "/api/v1/income/get",
+    DELETE_INCOME: (incomeId) => `/api/v1/income/${incomeId}`,
+    DELETE_ALL_INCOME: "/api/v1/income/all",
+    DOWNLOAD_INCOME: "/api/v1/income/downloadexcel",
   },
   EXPENSE: {
-    ADD_EXPENSE: "api/v1/expense/add",
-    GET_ALL_EXPENSE: "api/v1/expense/get",
-    DELETE_EXPENSE: (expenseId) => `api/v1/expense/${expenseId}`,
-    DELETE_ALL_EXPENSE: "api/v1/expense/all",
-    DOWNLOAD_EXPENSE: "api/v1/expense/downloadexcel",
+    ADD_EXPENSE: "/api/v1/expense/add",
+    GET_ALL_EXPENSE: "/api/v1/expense/get",
+    DELETE_EXPENSE: (expenseId) => `/api/v1/expense/${expenseId}`,
+    DELETE_ALL_EXPENSE: "/api/v1/expense/all",
+    DOWNLOAD_EXPENSE: "/api/v1/expense/downloadexcel",
   },
   IMAGE: {
-    UPLOAD_IMAGE: "api/v1/auth/upload-image",
+    UPLOAD_IMAGE: "/api/v1/auth/upload-image",
   },
 };
